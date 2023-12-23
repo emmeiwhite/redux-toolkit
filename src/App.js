72d04410ed9eafa1027912daa8257666a08b33fa
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-12-23 22:32:14 +0530

    Setting store in @redux/toolkit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import "./App.css";
 import MovieList from "./components/MovieList";
 import SongList from "./components/SongList";
 
 function App() {
   return (
-    <div className="app">
-      <button>Reset Both</button>
+    <div className="container">
+      <button className="reset-btn rounded-lg">Reset Both</button>
+      <hr />
       <MovieList />
+      <hr />
       <SongList />
     </div>
   );
